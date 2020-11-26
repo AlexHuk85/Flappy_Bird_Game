c00eac863c840f46f1f1bbce2f9fe44e92a38faa
Alex Huk <alex.huk85@gmail.com>
2020-11-26 18:05:45 +1100

    Game Over Done

--- game.py
+++ game.py
@@ -4,6 +4,8 @@ import pygame
 import sys
 import random
 
+pygame.font.init()
+
 
 # ---------------Function-----------------------
 def draw_floor():
@@ -54,9 +56,31 @@ def bird_animation():
     return new_bird, new_bird_rect
 
 
+def score_display(game_state):
+    if game_state == 'main_game':
+        score_surface = game_font.render(str(int(score)), True, (255, 255, 255))
+        score_rect = score_surface.get_rect(center=(288, 100))
+        screen.blit(score_surface, score_rect)
+    if game_state == 'game_over':
+        score_surface = game_font.render(f'Score: {(int(score))}', True, (255, 255, 255))
+        score_rect = score_surface.get_rect(center=(288, 100))
+        screen.blit(score_surface, score_rect)
+
+        high_score_surface = game_font.render(f'Higher score: {(int(high_score))}', True, (255, 255, 255))
+        high_score_rect = high_score_surface.get_rect(center=(288, 750))
+        screen.blit(high_score_surface, high_score_rect)
+
+
+def update_score(score, high_score):
+    if score > high_score:
+        high_score = score
+    return high_score
+
+
 # ------------Screen --------------------------
 screen = pygame.display.set_mode((576, 900))
 clock = pygame.time.Clock()
+game_font = pygame.font.Font('04b_19.ttf', 40)
 # ------------Background--------------
 bg_surface = pygame.image.load('assets/background-day.png').convert()
 bg_surface = pygame.transform.scale2x(bg_surface)
@@ -81,6 +105,10 @@ bird_rect = bird_surface.get_rect(center=(100, 500))
 gravity = 0.35  
 bird_movement = 0
 
+# -----------------Score -------------------------
+score = 0
+high_score = 0
+
 BIRDFLAP = pygame.USEREVENT + 1
 pygame.time.set_timer(BIRDFLAP, 200)
 
@@ -91,6 +119,9 @@ pipe_list = []
 pipe_height = [x for x in range(350, 650)]
 SPAWNPIPE = pygame.USEREVENT
 pygame.time.set_timer(SPAWNPIPE, 1200)
+
+game_over_surface = pygame.transform.scale2x(pygame.image.load('assets/message.png').convert_alpha())
+game_over_rect = game_over_surface.get_rect(center=(288, 450))
 
 game_active = True
 
@@ -110,6 +141,7 @@ while True:
                 pipe_list.clear()
                 bird_rect.center = (100, 450)
                 bird_movement = 0
+                score = 0
         # ---------For Bird Animation -----------
         if event.type == BIRDFLAP:
             if bird_index < 2:
@@ -144,6 +176,13 @@ while True:
             pipe_list.remove(pipe_list[0])
 
         game_active = check_collision(pipe_list)
+
+        score += 0.01
+        score_display('main_game')
+    else:
+        screen.blit(game_over_surface, game_over_rect)
+        high_score = update_score(score, high_score)
+        score_display('game_over')
 
     pygame.display.update()
     clock.tick(120)
